2a769c6966a168bf4151ccd3332d5dcf545d3f91
lzedan <zedanlouie@gmail.com>
2019-03-06 03:52:22 -0500

    Add files via upload

---
+++ FrogPlatforms.cs
@@ -0,0 +1,259 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using MoreMountains.InfiniteRunnerEngine;
+
+namespace FrogPlatforms {
+	/*-------       CLASS: DecoVariation       -------
+	Description: This is an individual variation of decor for a single platform.
+	  -------------------------*/
+	  [System.Serializable]
+	public class DecoVariation {
+		public bool isEnabled = true; //Set whether or not this variation can be spawned in the game
+		public int spawnOdds = 1; //Set how great of a chance this variation can occur specific to a platform
+		public GameObject decorSet; //The game object associated with the class
+
+		//Create a new DecoVariation object by assigning tiles
+		public DecoVariation() {
+		}
+
+		//SELECT DECOR
+		//This is the critical function. Selecting the decor will return the gameObject associated with
+		//the current DecoVariation object so that it can be turned on for an instanced platform.
+		public void selectDecor() {
+			if(this.isEnabled) {
+				this.decorSet.SetActive(true);
+			}
+			else{
+				throw new System.InvalidOperationException("GameObject must be enabled");
+			}
+		}
+	}
+
+
+	/*-------       CLASS: DecoOptions       -------
+	Description: A composite array of DecoVariation objects so that they don't have to be defined
+	per-platform per-instance.
+	  -------------------------*/
+	public class DecorOptions {
+		//Internal array of DecoVariation objects that are possible for a type of game platform
+		public DecoVariation[] availableOptions;
+
+		//A shit constructor that takes zero arguments. I just need to copy between component declaration and the start function, don't @ me
+		public DecorOptions() {
+		}
+
+		//Just in case the dev hasn't de-activated all the decor gameObjects, do it at initialisation with an input public set.
+		public DecorOptions(DecoVariation[] inputDecor) {
+			//Copy the input array into the internal array
+			this.availableOptions = inputDecor;
+
+			//Just in case, reset all DecoVariation GameObjects to inactive
+			for(int i = 0; i < this.availableOptions.Length; i++) {
+				this.availableOptions[i].decorSet.SetActive(false);
+			}
+		}
+
+		//I am absolutely not going to expose member variables. Check the length of the internal array.
+		public int reportLength() {
+			int output = 0;
+			//For some reason at start, the platforms don't have an availableOptions array yet.
+			//This if statement is here so that the programme doesn't freak out.
+			if(this.availableOptions == null) {
+				output = 0;
+			}
+			//We're fine now inside this else statement.
+			else {
+				output = this.availableOptions.Length;
+			}
+			return output;
+		}
+
+		public void selectVariation() {
+			//Handler for random number generation
+			System.Random rng = new System.Random();
+
+			//Builds an int array for consecutive values of maxChances to be associated with an index of a matching decor set
+			int[] chanceList = new int[this.availableOptions.Length];
+
+			int maxChances = 0;
+
+			//This first for loop is for determining the probability of certain sets of decorations appearing on an instanced platform.
+			//Add to maxChances to find the total probability of all sets
+			//And add the current maxChances to the chanceArray that associates the range of chance with a specific set of decorations
+			for(int i = 0; i < this.availableOptions.Length; i++) {
+				maxChances += this.availableOptions[i].spawnOdds;
+				chanceList[i] = maxChances;
+			}
+
+			//Generate a number that associates with a picked decor set
+			int selector = rng.Next(maxChances);
+
+			//THIS IS THE MAIN LOOP
+			//Iterate through the chanceList. This is mainly to iterate through the indexes of the associated decoration sets.
+			for(int j = 0; j < chanceList.Length; j++) {
+				//If our randomly generated number corresponds to a probability number at the current index,
+				//Activate the member GameObject with the member function
+				if(selector <= chanceList[j]){
+					try{
+						this.availableOptions[j].selectDecor();
+						break; //Break out of the loop - we don't need to continue and risk stacking decoration sets on the platform.
+					}
+					//If the selector generated number does not match to an active set,
+					//Reset all the variables so that we can continue finding a set to assign to the platform.
+					catch(System.InvalidOperationException) {
+						j = 0;
+						selector = rng.Next(maxChances);
+						continue;
+					}
+				}
+			}
+
+		}
+
+		//RESET FUNCTION
+		//Once a platform goes out of bounds, de-activate all GameObjects associated with DecoVariation objects in the internal array.
+		//Only then, re-start the process for picking a new decoration set.
+		public void resetVariation() {
+			//When a platform gets deactivated at runtime by going out of bounds, turn off all active sets and re-select a Variation
+			for(int i = 0; i < this.availableOptions.Length; i++) {
+				this.availableOptions[i].decorSet.SetActive(false);
+			}
+
+			//Use the member function selectVariation to pick a new decoration set for the platform once it's re-entered into the game.
+			this.selectVariation();
+		}
+	}
+
+	/*-------       CLASS: EntityObject      -------
+	Description: This is an individual entity object in-game that is inherently dynamic and affects
+	points or player condition. Automatically activated depending on the ID code generated during
+	platform instancing.
+	  -------------------------*/
+	  [System.Serializable]
+	public class EntityObject {
+		public System.String entityType = "coin";  //Type of entity, whether this is a coin, enemy, pickup, or obstacle
+		public float pointsToAdd = 1; //How many points to add depending on type of interaction behaviour
+		public int pointsCollection = 1; //If 0, add to generic points collector. Otherwise, add to coin aggregate
+		public bool isEnabled = true; //Is this entity enabled for generation?
+		public GameObject entityGO; //The GameObject associated with this individual object
+
+		//Creation function for a new EntityObject
+		public EntityObject() {
+		}
+
+		//Second Creation function that actively takes inputs so we're no longer on planet empty
+		public EntityObject(System.String eType, float assoPoints, int wherego, GameObject currObject) {
+			this.entityType = eType;
+			this.pointsToAdd = assoPoints;
+			this.pointsCollection = wherego;
+			this.entityGO = currObject;
+		}
+
+		//Depending on the value of pointsCollection, this function adds points to the proper collection
+		//As determined by the interaction behaviour defined in a separate script.
+		public void addPoints() {
+			GameObject gameManager = GameObject.Find("GameManager");
+			if(pointsCollection == 0){
+				gameManager.GetComponent<GameManager>().SendMessage("AddPoints", pointsToAdd);
+			}
+			else if(pointsCollection == 1){
+				gameManager.GetComponent<GameManager>().SendMessage("AddCoins", (int)pointsToAdd);
+			}
+		}
+
+		//EntityScene generates a random number for the platform called an ID
+		//This ID can be calculated to activate certain types of entityObjects for the dynamic part of the scene.
+		//Each entity type has a unique seeding function.
+		public void seedActivation(int generatorID, int idRange) {
+			if(this.isEnabled == true) {
+				if(entityType == "coin") { //Roughly 42% of all ID's will activate coins on the platform
+					if(generatorID > ((idRange/2)+(0.08*idRange))) {
+						this.entityGO.SetActive(true);
+					}
+					else{
+						this.entityGO.SetActive(false);
+					}
+				}
+				else if(entityType == "enemy") { //Roughly 29% of all ID's will activate enemies on the platform
+					if((generatorID % 3 == 0) && (generatorID % 7 >= 1)) {
+						this.entityGO.SetActive(true);
+					}
+					else{
+						this.entityGO.SetActive(false);
+					}
+				}
+				else if(entityType == "pickup") { //Roughly 9% of all ID's will activate pickups on the platform
+					if(generatorID % 11 == 0) {
+						this.entityGO.SetActive(true);
+					}
+					else{
+						this.entityGO.SetActive(false);
+					}
+				}
+				else if(entityType == "obstacle") { //Roughly 20% of all ID's will generate any obstacles on the platform
+					if((generatorID + 3) % 5 == 0) {
+						this.entityGO.SetActive(true);
+					}
+					else{
+						this.entityGO.SetActive(false);
+					}
+				}
+			}
+		}
+
+		//When the collector EntityScene resets, it uses this de-activate function so it doesn't access member variables
+		public void deactivateEntity() {
+			this.entityGO.SetActive(false);
+		}
+
+
+	}
+
+	/*-------       CLASS: EntityScene       -------
+	Description: A composite array of EntityObject objects that is associated with a tile layout
+	platform style.
+	  -------------------------*/
+	public class EntityScene {
+		public EntityObject[] availableEntities;
+		public int id_range = 255;
+		//public int generated_id = 10;
+
+		public EntityScene() {
+			//Nothing lol, just here for a copy argument
+		}
+
+		public EntityScene(EntityObject[] inputEntities) {
+			this.availableEntities = inputEntities;
+		}
+
+		public void objectActivation(int inputRange) {
+			//Handler for random number generation
+			System.Random rng = new System.Random();
+
+			this.id_range = inputRange;
+
+			int selector = rng.Next(this.id_range);
+			for(int v = 0; v < this.availableEntities.Length; v++) {
+				this.availableEntities[v].seedActivation(selector, this.id_range);
+			}
+		}
+
+		//Still need a reset function lol
+		public void resetScene() {
+			if(this.availableEntities == null){
+				return;
+			}
+			else{
+				int entityListLength = this.availableEntities.Length;
+				for(int i = 0; i < entityListLength; i++) {
+					availableEntities[i].deactivateEntity();
+				}
+
+				this.objectActivation(this.id_range);
+			}
+		}
+	}
+
+}
---
+++ PixelDisplay_Helper.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelDisplay_Helper : MonoBehaviour {
+
+	public int native_Width = 0;
+	public int native_Height = 368;
+
+	GameObject debugAgent;
+
+	public float pixelsPerUnit = 16f;
+	//public int wiggleRange = 8;
+
+	Vector3 cameraPos = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		debugAgent = GameObject.Find("DebugPanel UI");
+		WindowSetup();
+		ApplyZoom();
+		AdjustCamera();
+		//WiggleValues();
+	}
+
+	public void AdjustCamera() {
+		Camera.main.transform.position = new Vector3(
+			RoundToNearestPixel(cameraPos.x),
+			RoundToNearestPixel(cameraPos.y),
+			-10f
+		);
+	}
+
+	public float RoundToNearestPixel(float pos) {
+		float screenPixelsPerUnit = Screen.height / (Camera.main.orthographicSize * 2f);
+		float pixelValue = Mathf.Round(pos * screenPixelsPerUnit);
+
+		return pixelValue / screenPixelsPerUnit;
+	}
+
+	public void ApplyZoom() {
+		float smallestDimension = Screen.height < Screen.width ? Screen.height : Screen.width;
+		float pixelScale = Mathf.Clamp(Mathf.Round(smallestDimension / native_Height), 1f, 8f);
+
+		Camera.main.orthographicSize = (Screen.height / (pixelsPerUnit * pixelScale)) * 0.5f;
+	}
+
+	/*void WiggleValues() {
+		//This function is being weird. It's meant to approximate within a range of values, what would scale the best for a proposed native height
+		//Right now it somehow decides that the last number is always best. I'll need to find a way to detect which values have the most zeroes 
+		//after the decimal place. Or, at least, closest to becoming a zero.
+
+		float closestLow = 1.0f;
+		float subNumber = 0.0f;
+		float proposedValue = 1.0f;
+		int optimalDimension = 0;
+
+		for(int currValue = (native_Height - wiggleRange); currValue <= (native_Height + wiggleRange); currValue++) {
+			proposedValue = Screen.height/(float)currValue;
+			Debug.Log(proposedValue);
+			subNumber = proposedValue - (int)proposedValue;
+			if(subNumber <= closestLow){
+				closestLow = subNumber;
+				optimalDimension = currValue;
+			}
+		}
+
+		Debug.Log(optimalDimension);
+	}*/
+
+	void WindowSetup() {
+		//First values
+		//First calculate how many native screens we can fit into the full screen based on height.
+		//This number will inevitably be a decimal, so we need to compare both the lower value with the decimals truncated
+		//and that value + 1
+		float compareBase = Screen.height/native_Height;
+		float Low_option = (float)(System.Math.Truncate(compareBase)); //The Truncate function just knocks out whatever values follow the decimal point.
+		float High_option = (float)(System.Math.Truncate(compareBase) + 1.0f);
+
+		//Setting up values that will be used in our for loop
+		int diff1 = 0;
+		int diff2 = 0;
+		bool legitValues = true; //Assuming that all our calculations will result in usable numbers
+		int output = 0;
+
+		//This for loop sort of filters through our options values we calculated earlier. We need to find out which resolution
+		//best suits the kind of game we're running, based on the available screen resolution output by the display.
+		//We're iterating through a range of (0, 2) because there's literally only two options.
+		for(int i = 0; i < 2; i++) {
+			//The first option, the lower multiple, Low_option
+			if(i == 0) {
+				//CHECK: Will this multiple, if applied to our native height, be greater than what our display screen is capable of?
+				if((Low_option * native_Height) > Screen.height) {
+					legitValues = false;
+					continue;
+				}
+				//Otherwise, calculate what the difference is between this value and our native height
+				//We want whatever value has the lowest absolute value
+				else{
+					diff1 = Mathf.Abs(native_Height - ((int)Low_option*native_Height));
+				}
+			}
+
+			//The second option, the higher multiple, High_option
+			if(i == 1) {
+				//CHECK: Will this multiple, if applied to our native height, be greater than what our display screen is capable of?
+				if((High_option * native_Height) > Screen.height) {
+					legitValues = false;
+					continue;
+				}
+				//Otherwise, calculate what the difference is between this value and our native height
+				//We want whatever value has the lowest absolute value
+				else{
+					diff2 = Mathf.Abs(native_Height - ((int)High_option*native_Height));
+				}
+			}
+		}
+
+		//Okay cool, so we have the absolute value differences for both options.
+		//NOW CHECK: Which has the lowest absolute value? The lowest absolute value represents the option closest to our native height.
+		//If both values are compliant in that they don't extend beyond our screen's capabilities, set this value as our output.
+		if(legitValues) {
+			if(diff1 < diff2) {
+				output = (int)Low_option*native_Height;
+			}
+			else if(diff2 > diff1) {
+				output = (int)High_option*native_Height;
+			}
+		}
+		//If one of our options is not compliant, find whatever value is greater than zero.
+		else{
+			if(diff1 > 0) {
+				output = (int)Low_option*native_Height;
+			}
+			else if(diff2 > 0) {
+				output = (int)High_option*native_Height;
+			}
+		}
+
+		//Our last step is to find the width.
+		//REMEMBER, the important dimension is height, so the width is whatever complies with our screen's aspect ratio so that the
+		//game view fills the whole available space.
+		//With that in mind, multiply the output to our aspect ratio to get the width.
+		//debugAgent.GetComponent<DebugHandler>().SendMessage("adjustedHeight", output);
+		int new_Width = Mathf.RoundToInt((Screen.width*output)/Screen.height);
+		//debugAgent.GetComponent<DebugHandler>().SendMessage("adjustedWidth", new_Width);
+		//Set the resolution with our new values. We did it!
+		Screen.SetResolution(new_Width, output, true);
+
+	}
+}
